8bd09da7bdfd45654cc9685de89d8c6f2fda4b2b
pySoo <soopy368@gmail.com>
2023-10-24 11:09:58 +0900

    [feat]: naver search shop api 통신 파일 추가

---
+++ graphql/shoppingAPI.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+
+const API_URL = 'https://openapi.naver.com/v1/search/shop.json';
+
+export const getShoppingList = (keyword) => {
+  if (!keyword || keyword.trim() === '') {
+    throw new Error('키워드를 입력해 주세요.');
+  }
+
+  return fetch(`${API_URL}?query=${keyword}`, {
+    method: 'get',
+    headers: {
+      'X-Naver-Client-Id': process.env.API_CLIENT_ID,
+      'X-Naver-Client-Secret': process.env.API_CLIENT_SECRET,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`API 호출에 실패했습니다. 에러 상태: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((json) => json.items);
+};
