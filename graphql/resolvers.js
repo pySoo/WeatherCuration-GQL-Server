97254d8604bb195aa3134b66efb65296b4a47b8f
pySoo <soopy368@gmail.com>
2023-10-26 07:00:33 +0900

    [feat] : 좌표 변환, 시간 예보 API 제거

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,18 +1,11 @@
 import { getShoppingList } from './shoppingAPI.js';
-import {
-  getCoordLocation,
-  getCurrentCondition,
-  getFiveDaysForecast,
-  getTwelveHoursForecast,
-} from './weatherAPI.js';
+import { getCurrentCondition, getFiveDaysForecast } from './weatherAPI.js';
 
 const resolvers = {
   Query: {
-    shoppingList: (_, { keyword }) => getShoppingList(keyword),
-    coordLocation: (_, { coordinates }) => getCoordLocation(coordinates),
-    currentCondition: (_, { locationKey }) => getCurrentCondition(locationKey),
-    fiveDaysForecast: (_, { locationKey }) => getFiveDaysForecast(locationKey),
-    twelveHoursForecast: (_, { locationKey }) => getTwelveHoursForecast(locationKey),
+    getShoppingList: (_, { keyword }) => getShoppingList(keyword),
+    getCurrentCondition: (_, { locationKey }) => getCurrentCondition(locationKey),
+    getFiveDaysForecast: (_, { locationKey }) => getFiveDaysForecast(locationKey),
   },
 };
 
--- graphql/weatherAPI.js
+++ graphql/weatherAPI.js
@@ -7,26 +7,16 @@ const weatherFetch = ({ params, endPoint, method = 'get' }) => {
 
   return fetch(`${API_URL}${endPoint}?${weatherParams.toString()}`, {
     method,
-  }).then((res) => {
-    if (!res.ok) {
-      throw new Error(`API 호출에 실패했습니다. 에러 상태: ${res.status}`);
-    }
-    return res.json();
-  });
-};
-
-export const getCoordLocation = ({ lat, lng }) => {
-  const params = {
-    apikey: process.env.ACCUWEATHER_API_KEY,
-    q: `${lat},${lng}`,
-    language: 'ko',
-    details: 'true',
-    toplevel: 'true',
-  };
-
-  const endPoint = '/locations/v1/cities/geoposition/search';
-
-  return weatherFetch({ params, endPoint });
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`API 호출에 실패했습니다. 에러 상태: ${res.status}`);
+      }
+      return res.json();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 export const getCurrentCondition = async (locationKey) => {
@@ -51,19 +41,6 @@ export const getFiveDaysForecast = (locationKey) => {
   };
 
   const endPoint = `/forecasts/v1/daily/5day/${locationKey}`;
-
-  return weatherFetch({ params, endPoint });
-};
-
-export const getTwelveHoursForecast = (locationKey) => {
-  const params = {
-    apikey: process.env.ACCUWEATHER_API_KEY,
-    language: 'ko',
-    details: 'true',
-    metric: 'true',
-  };
-
-  const endPoint = `/forecasts/v1/hourly/12hour/${locationKey}`;
 
   return weatherFetch({ params, endPoint });
 };
