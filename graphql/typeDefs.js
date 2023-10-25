3bae2b93000402e7e0567442e68fc65291953707
pySoo <soopy368@gmail.com>
2023-10-26 07:01:19 +0900

    [feat] : 사용하지 않는 쿼리 제거

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,24 +1,16 @@
 import { gql } from 'apollo-server';
 
 const typeDefs = gql`
-  type Region {
-    ID: String
-    LocalizedName: String
-    EnglishName: String
+  type ShoppingItem {
+    title: String!
+    link: String!
+    image: String!
+    lprice: String!
   }
 
-  type Country {
-    ID: String
-    LocalizedName: String
-    EnglishName: String
-  }
-
-  type TimeZone {
-    Code: String
-    Name: String
-    GmtOffset: Float
-    IsDaylightSaving: Boolean
-    NextOffsetChange: String
+  type ShoppingList {
+    keyword: String!
+    shoppingData: [ShoppingItem]
   }
 
   type Metric {
@@ -34,68 +26,36 @@ const typeDefs = gql`
     UnitType: Int
   }
 
-  type ImperialValue {
-    Value: Float
-    Unit: String
-    UnitType: Int
-  }
-
-  type Elevation {
-    Metric: MetricValue
-    Imperial: ImperialValue
-  }
-
-  type Imperial {
-    Value: Float
-    Unit: String
-    UnitType: Int
-    Phrase: String
-  }
-
   type Values {
     Metric: Metric
-    Imperial: Imperial
+  }
+
+  type PrecipitationSummary {
+    Precipitation: Values
+    PastHour: Values
+    Past3Hours: Values
+    Past6Hours: Values
+    Past9Hours: Values
+    Past12Hours: Values
+    Past18Hours: Values
+    Past24Hours: Values
+  }
+
+  type CurrentConditions {
+    IsDayTime: Boolean
+    CloudCover: Int
+    PrecipitationType: String
+    PrecipitationSummary: PrecipitationSummary
+    PastHour: Values
+    UVIndex: Int
+    Temperature: Values
+    WeatherText: String
+    RelativeHumidity: Int
   }
 
   type Temperature {
     Minimum: Metric
     Maximum: Metric
-  }
-
-  type GeoPosition {
-    Latitude: Float
-    Longitude: Float
-    Elevation: Elevation
-  }
-
-  type Location {
-    id: String
-    Version: Int
-    Key: String
-    Type: String
-    Rank: Int
-    LocalizedName: String
-    EnglishName: String
-    PrimaryPostalCode: String
-    Region: Region
-    Country: Country
-    TimeZone: TimeZone
-    GeoPosition: GeoPosition
-    IsAlias: Boolean
-    DataSets: [String]
-  }
-
-  input CoordinatesInput {
-    lat: Float!
-    lng: Float!
-  }
-
-  type AirAndPollen {
-    Name: String
-    Value: Int
-    Category: String
-    CategoryValue: Int
-    Type: String
   }
 
   type Day {
@@ -108,84 +68,35 @@ const typeDefs = gql`
     HoursOfRain: Float
     HoursOfSnow: Float
     HoursOfIce: Float
+    PrecipitationType: String
+    PrecipitationProbability: Int
+    CloudCover: Int
+    ThunderstormProbability: Int
   }
 
-  type CurrentConditions {
-    locationKey: String!
-    LocalObservationDateTime: String
-    EpochTime: Int
-    WeatherText: String
-    WeatherIcon: Int
-    HasPrecipitation: Boolean
-    PrecipitationType: String
-    IsDayTime: Boolean
-    Temperature: Values
-    RealFeelTemperature: Values
-    RelativeHumidity: Int
-    Past24HourTemperatureDeparture: Values
-    MobileLink: String
-    Link: String
+  type Sun {
+    Rise: String
+    EpochRise: Int
+    Set: String
+    EpochSet: Int
   }
 
   type DailyForecast {
     Date: String
-    EpochDate: Int
     Temperature: Temperature
-    RealFeelTemperature: Temperature
-    AirAndPollen: [AirAndPollen]
     Day: Day
     Night: Day
-    Sources: [String]
-    MobileLink: String
-    Link: String
+    Sun: Sun
   }
 
-  type FiveDaysForecast {
-    locationKey: String!
+  type FiveDaysForecasts {
     DailyForecasts: [DailyForecast!]!
   }
 
-  type HoursForecast {
-    DateTime: String
-    EpochDateTime: Int
-    WeatherIcon: Int
-    IconPhrase: String
-    HasPrecipitation: Boolean
-    PrecipitationType: String
-    PrecipitationIntensity: String
-    IsDaylight: Boolean
-    Temperature: Metric
-    RealFeelTemperature: Metric
-    RelativeHumidity: Int
-    UVIndex: Int
-    UVIndexText: String
-    RainProbability: Int
-    SnowProbability: Int
-    IceProbability: Int
-    TotalLiquid: Metric
-    MobileLink: String
-    Link: String
-  }
-
-  type TwelveHoursForecast {
-    locationKey: String!
-    HoursForecasts: [HoursForecast!]!
-  }
-
-  type ShoppingItems {
-    title: String!
-    link: String!
-    image: String!
-    lprice: String!
-    category1: String!
-  }
-
   type Query {
-    shoppingList(keyword: String!): [ShoppingItems]
-    coordLocation(coordinates: CoordinatesInput!): Location!
-    currentCondition(locationKey: String!): CurrentConditions!
-    fiveDaysForecast(locationKey: String!): FiveDaysForecast!
-    twelveHoursForecast(locationKey: String!): TwelveHoursForecast!
+    getShoppingList(keyword: String!): ShoppingList!
+    getCurrentCondition(locationKey: String!): CurrentConditions!
+    getFiveDaysForecast(locationKey: String!): FiveDaysForecasts!
   }
 `;
 
