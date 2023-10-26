5d0d87ef09f1a7eae5bbbff69932d27cb82ed48d
pySoo <soopy368@gmail.com>
2023-10-26 13:17:06 +0900

    [feat] : cors origin 설정

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ import { ApolloServer } from 'apollo-server';
 import typeDefs from './graphql/typeDefs.js';
 import resolvers from './graphql/resolvers.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  cors: {
+    origin: '*',
+  },
+  typeDefs,
+  resolvers,
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
